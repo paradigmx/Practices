c79e76745a7402d6576e7b0e24740985d28a6f8c
Neo Lee <neo.lee@gmail.com>
2014-07-09 01:22:58 +0800

    WIP: ISO-8601

--- Practices/ISO8601ViewController.m
+++ Practices/ISO8601ViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "ISO8601ViewController.h"
+#import "ISO8601DateFormatter.h"
 
 @interface ISO8601ViewController ()
+- (IBAction)parse:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UITextField *dateTextField;
+@property (weak, nonatomic) IBOutlet UILabel *labelYear;
+@property (weak, nonatomic) IBOutlet UILabel *labelMonth;
+@property (weak, nonatomic) IBOutlet UILabel *labelDay;
+@property (weak, nonatomic) IBOutlet UILabel *labelHour;
+@property (weak, nonatomic) IBOutlet UILabel *labelMinute;
+@property (weak, nonatomic) IBOutlet UILabel *labelSecond;
+@property (weak, nonatomic) IBOutlet UILabel *labelTZ;
 @end
 
 @implementation ISO8601ViewController
@@ -18,7 +28,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+
     }
     return self;
 }
@@ -26,7 +36,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [_dateTextField setText:@"2013-06-05T14:10:43.678Z"];
+    [_dateTextField setText:@"2013-02-03T19:54:00-0800"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,4 +58,30 @@
 }
 */
 
+- (IBAction)parse:(id)sender {
+    NSString *dateString = [[_dateTextField text] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+
+    ISO8601DateFormatter *formatter = [[ISO8601DateFormatter alloc] init];
+    NSTimeZone *tz;
+    NSDate *date = [formatter dateFromString:dateString timeZone:&tz];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+
+    if (tz) {
+        [gregorian setTimeZone:tz];
+    }
+
+    unsigned unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSTimeZoneCalendarUnit;
+    NSDateComponents *components = [gregorian components:unitFlags fromDate:date];
+
+    [_labelYear setText:[@([components year]) stringValue]];
+    [_labelMonth setText:[@([components month]) stringValue]];
+    [_labelDay setText:[@([components day]) stringValue]];
+    [_labelHour setText:[@([components hour]) stringValue]];
+    [_labelMinute setText:[@([components minute]) stringValue]];
+    [_labelSecond  setText:[@([components second]) stringValue]];
+
+    tz = [components timeZone];
+    [_labelTZ setText:[[tz name] stringByAppendingFormat:@"(%@)", [tz abbreviation]]];
+}
 @end
