a0d73f655fb202d778b4e68c8abe937fad38c32e
Neo Lee <neo.lee@gmail.com>
2014-07-15 15:56:58 +0800

    SuperPrime

--- Practices/SuperPrimeViewController.m
+++ Practices/SuperPrimeViewController.m
@@ -50,18 +50,55 @@
 }
 */
 
+// Helper methods
+- (BOOL)isPrime:(int)n {
+    if (n <= 1) return false;
+    if (n % 2 == 0) return 1;
+    for (int i = 3; i < floor(sqrt(n)); i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+- (NSString *)stringFromCArray:(int *)numbers length:(int)length {
+    NSString *result = @"[";
+
+    for (int i = 0; i < length; i++) {
+        result = [result stringByAppendingFormat:@"%d", numbers[i]];
+        if (i < length-1) {
+            result = [result stringByAppendingString:@","];
+        }
+    }
+
+    return [result stringByAppendingString:@"]"];
+}
+
 - (IBAction)compute:(id)sender {
+    int MAGIC_LENGTH = 11;
+
     primesieve_iterator pi;
     primesieve_init(&pi);
-    uint64_t sum = 0;
+
     uint64_t prime = 0;
+    int *primes = nil;
+    int sum = 0;
 
     // iterate over the primes below 10^10
-    while ((prime = primesieve_next_prime(&pi)) < 10000000000ull)
-        sum += prime;
+    while ((prime = primesieve_next_prime(&pi)) < 10000000000ull) {
+        primes = (int*) primesieve_generate_n_primes(MAGIC_LENGTH, prime, INT_PRIMES);
+        sum = 0;
+        for (int i = 0; i < MAGIC_LENGTH; i++) {
+            sum += primes[i];
+        }
+        if (sum <= 2011) continue;
+        if ([self isPrime:sum]) break;
+    }
+
     primesieve_free_iterator(&pi);
 
-    NSString *output = [NSString stringWithFormat:@"Sum of the primes below 10^10 = %llu\n", sum];
+    NSString *output = [NSString stringWithFormat:@"The next Magic Prime is: %d <- %@", sum, [self stringFromCArray:primes length:MAGIC_LENGTH]];
 
     [_outputTextView setText:output];
 }
